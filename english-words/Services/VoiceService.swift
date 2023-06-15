1764a1c22c8262944061ba139bdfd10b28668cf3
serezha <koposov.serezha@gmail.com>
2023-06-15 19:27:13 +0300

    small edits

--- english-words/Services/VoiceService.swift
+++ english-words/Services/VoiceService.swift
@@ -8,16 +8,36 @@
 import Foundation
 import AVFoundation
 
-final class VoiceService {
+final class VoiceService: NSObject {
 
     static let shared = VoiceService()
 
+    let synthesizer = AVSpeechSynthesizer()
+
+    var isSpeaking: Bool = false
+
+    private override init() {
+        super.init()
+        synthesizer.delegate = self
+    }
+
     func speakWord(word: String) {
-        let synthesizer = AVSpeechSynthesizer()
         let utterance = AVSpeechUtterance(string: word)
         utterance.voice = AVSpeechSynthesisVoice(language: "com.apple.eloquence.en-US.Eddy")
 
-        synthesizer.speak(utterance)
+        if !isSpeaking {
+            synthesizer.speak(utterance)
+        }
+    }
+}
+
+extension VoiceService: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        isSpeaking = true
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        isSpeaking = false
     }
 }
 
